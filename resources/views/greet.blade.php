40092cd8818fcfadce7ea088c761c41c61650675
half-25 <cerithhalfpenny@gmail.com>
2025-11-25 08:41:02 +0000

    Initial Commit

---
+++ resources/views/greet.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <h1>Hello, {{ $name}}!</h1>
+    <p>Welcome to Axolotl Legends. Share your latest hijinks!</p>
+</body>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="/styles.css"> 
+    <title> Axolotl Legends </title>
+</head>
+
+<body>
+    <h1> Welcome to Axolotl Legends </h1>
+    <p> A place for sharing legendary tales of pesky axolotl's </p>
+    <img src="/Axolotl.jpg" alt="A fine example of an Axolotl  with a cheeky smile" width="300" height="200"> 
+    <p><small> Image source: Wikipedia Commons</small></p> 
+</body>
+</html>
