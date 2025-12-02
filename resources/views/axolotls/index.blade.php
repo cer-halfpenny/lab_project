27a591f8230a59303dae57c7a3dba69e2b6a0a98
half-25 <cerithhalfpenny@gmail.com>
2025-12-02 09:38:12 +0000

    Adding text box to be rendered, (HTML)

--- resources/views/axolotls/index.blade.php
+++ resources/views/axolotls/index.blade.php
@@ -7,6 +7,14 @@
 <body>
     <h1>Axolotls</h1>
 
+    // Creating the text box the user types in, and an empty <ul> where we’ll show suggestions.
+    <div>
+        <label for="axolotl-search">Search by name:</label>
+        <input type="text" id="axolotl-search" autocomplete="off">
+
+        <ul id="axolotl-suggestions"></ul>
+    </div>
+
     <h2>All Axolotls</h2>
     <ul>
         @foreach ($axolotls as $axolotl)
