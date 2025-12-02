a40970ac3894911b2e2134f6a8fa8d4bd11e7d95
half-25 <cerithhalfpenny@gmail.com>
2025-12-02 08:34:56 +0000

    closed operation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,4 +25,4 @@ class DatabaseSeeder extends Seeder
         \App\Models\Axolotl::factory(8)->create();   // 3 random axolotls
         \App\Models\Story::factory(15)->create();     // 5 random stories (each linked to a NEW axolotl)
     }
-}
+} 
